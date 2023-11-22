43635240731e9ef54d7214eef779c803d2fc79ff
Zemorath <trentward10@gmail.com>
2023-11-21 19:21:03 -0600

    Sequences complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    a, b = 0, 1
+    count = 0
+    list = []
+
+    if length <= 0:
+        print([])
+    elif length == 1:
+        list.append(0)
+        print(list)
+    else:
+        while count < length:
+            list.append(a)
+            c = a + b
+            a = b
+            b = c
+            count+=1
+        print(list)
